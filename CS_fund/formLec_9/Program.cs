d8ef130f0f3deefcf397cfc3a648780f126bda69
Costa033 <abdoessam746@gmail.com>
2024-10-29 19:41:39 +0300

    my lecture 9 to 16 files

--- CS_fund/formLec_9/Program.cs
+++ CS_fund/formLec_9/Program.cs
@@ -1,14 +1,160 @@
-﻿namespace formLec_9
+﻿using System.Numerics;
+
+namespace formLec_9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            /*
+            Console.Title = "Welcome man";
             string name;
             Console.Write("please enter your name\t");
             name=Console.ReadLine();
             Console.WriteLine($"Hello, {name} !");
+
+            Console.WriteLine($"the length of ur name is {name.Length }\t");
+
+            Console.WriteLine($"Name to upper case\t {name.ToUpper()}");
+            Console.WriteLine($"Name to lower case\t {name.ToLower()}");
+
+            Console.WriteLine(name.StartsWith("Co"));
+
+            Console.WriteLine(name.StartsWith("co"));
+            Console.WriteLine(name.StartsWith("coSTA", StringComparison.OrdinalIgnoreCase));    //never mind if it is Upper or Lower 
+
+            Console.WriteLine(name.EndsWith("KE"));
+            Console.WriteLine(name.EndsWith("keKe", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(name.Contains("Os"));
+            Console.WriteLine(name.Contains("OS", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(name.Replace("Costa", "Essam"));
+            Console.WriteLine(name.Replace("Costa", "Essam",StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("\n\n*****************************\n");
+
+            string myName = " ";
+            Console.Write("please enter your name :\t");
+            myName = Console.ReadLine();
+            Console.Write($"Hello {myName}, please enter the name you want to replace:");
+            string OldName = Console.ReadLine();
+            Console.Write("Hello please enter the name you want to replace With:");
+            string NewName = Console.ReadLine();
+
+            Console.WriteLine(myName.Replace(OldName, NewName,StringComparison.OrdinalIgnoreCase)+"\t^_^");
+
+            */
+
+
+            /*
+            //Lecture 10
+
+            // int a = 0;
+            // int b = 50;
+            // int c = -50;
+
+            //uint ua = 0;
+            //uint ub = 50;
+            ////uint uc = -50;
+            ///
+
+
+
+            //int           long            Double                Ushort  
+            //Uint          Ulong           float                 short              
+
+            // 1-Memory size      2-Maximum value       3-minimum value 
+
+            //int
+            Console.WriteLine($"the memory size of int is : {sizeof(int)}");
+            Console.WriteLine($"the minimum value integer can store {int.MinValue}");
+            Console.WriteLine($"the Maximum value integer can store {int.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+
+            //uint
+            Console.WriteLine($"the memory size of uint is : {sizeof(uint)}");
+            Console.WriteLine($"the minimum value uinteger can store {uint.MinValue}");
+            Console.WriteLine($"the Maximum valueu integer can store {uint.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+
+
+            //Double
+            Console.WriteLine($"the memory size of double is : {sizeof(double)}");
+            Console.WriteLine($"the minimum value Double can store {double.MinValue}");
+            Console.WriteLine($"the Maximum value Double can store {double.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+
+            //Float
+            Console.WriteLine($"the memory size of float is : {sizeof(float)}");
+            Console.WriteLine($"the minimum value Float can store {float.MinValue}");
+            Console.WriteLine($"the Maximum value Float can store {float.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+
+
+            //Long
+            Console.WriteLine($"the memory size of long is : {sizeof(long)}");
+            Console.WriteLine($"the minimum value Long can store {long.MinValue}");
+            Console.WriteLine($"the Maximum value Long can store {long.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+
+            //uLong
+            Console.WriteLine($"the memory size of ulong is : {sizeof(ulong)}");
+            Console.WriteLine($"the minimum value uLong can store {ulong.MinValue}");
+            Console.WriteLine($"the Maximum value uLong can store {ulong.MaxValue}");
+
+            Console.WriteLine("****************************************************************");
+            */
+
+
+
+            //Lecture 11
+
+
+            //Lecture 12
+
+
+            //Lecture 13
+
+
+            //Lecture 14
+
+
+            //Lecture 15
+
+
+            //Lecture 16
+
+            /*
+            // string -> int
+            Console.Write("please enter your birth year :\t");
+            string year = Console.ReadLine();
+            int x = int.Parse(year);
+            Console.WriteLine($"your age now in 2024 is : {2024 - x}");
+
+
+            // string -> Double
+            Console.Write("please enter your birth year :\t");
+            string year_ = Console.ReadLine();
+            double y = double.Parse(year_);
+            Console.WriteLine($"your age now in 2024 is : {2024 - y}");
+            */
+
+
+            //Lecture 17
+
+
+
+
+
+
+
+
         }
     }
 }
